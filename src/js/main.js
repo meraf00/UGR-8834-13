55bb5a16804bdaeb146357a16f115f49762779a3
meraf00 <meraf000@gmail.com>
2023-01-14 00:55:57 +0300

    Completed styling

--- src/js/main.js
+++ src/js/main.js
@@ -25,3 +25,40 @@ courses.forEach((course) => {
     });
   }
 });
+
+// toggle audio transcript
+const showHideButtons = document.querySelectorAll(".toggle-transcript");
+
+showHideButtons.forEach((button) => {
+  if (button.dataset.target) {
+    button.addEventListener("click", () => {
+      const transcriptElement = document.getElementById(button.dataset.target);
+      button.innerHTML = "Hide Transcript";
+      if (transcriptElement.classList.contains("section--hidden")) {
+        button.innerHTML = "Hide Transcript";
+      } else {
+        button.innerHTML = "Show Transcript";
+      }
+      transcriptElement.classList.toggle("section--hidden");
+      button.blur();
+    });
+  }
+});
+
+// Tab navigation
+const tabNavigationButtons = document.querySelectorAll(".tab__button");
+tabNavigationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    tabNavigationButtons.forEach((btn) => {
+      btn.classList.toggle("tab__button--active", false);
+
+      let target = document.getElementById(btn.dataset.target);
+      target.classList.toggle("tab__content--hidden", true);
+    });
+
+    button.classList.toggle("tab__button--active", true);
+
+    let target = document.getElementById(button.dataset.target);
+    target.classList.toggle("tab__content--hidden", false);
+  });
+});
