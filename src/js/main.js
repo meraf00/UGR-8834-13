4b9d7073d46d3ee067c8af2fce5144b2376477db
meraf00 <meraf000@gmail.com>
2023-01-19 21:46:14 +0300

    Restyled UI with bootstrap

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,3 @@
-// move label on input focus
-const inputs = document.querySelectorAll("input");
-
-inputs.forEach((input) => {
-  if (input.dataset.label) {
-    input.addEventListener("focus", () => {
-      const label = document.getElementById(input.dataset.label);
-      label.classList.toggle("search__input-label--hidden", true);
-    });
-    input.addEventListener("focusout", () => {
-      const label = document.getElementById(input.dataset.label);
-      if (input.value) return;
-      label.classList.toggle("search__input-label--hidden", false);
-    });
-  }
-});
-
 // make course cards clickable
 const courses = document.querySelectorAll(".course");
 
@@ -26,50 +9,22 @@ courses.forEach((course) => {
   }
 });
 
-// toggle audio transcript
-const showHideButtons = document.querySelectorAll(".toggle-transcript");
-
-showHideButtons.forEach((button) => {
-  if (button.dataset.target) {
-    button.addEventListener("click", () => {
-      const transcriptElement = document.getElementById(button.dataset.target);
-      button.innerHTML = "Hide Transcript";
-      if (transcriptElement.classList.contains("section--hidden")) {
-        button.innerHTML = "Hide Transcript";
-      } else {
-        button.innerHTML = "Show Transcript";
-      }
-      transcriptElement.classList.toggle("section--hidden");
-      button.blur();
-    });
-  }
-});
-
 // Tab navigation
-const tabNavigationButtons = document.querySelectorAll(".tab__button");
+const tabNavigationButtons = document.querySelectorAll(".tab-button");
 tabNavigationButtons.forEach((button) => {
   button.addEventListener("click", () => {
     tabNavigationButtons.forEach((btn) => {
-      btn.classList.toggle("tab__button--active", false);
+      btn.classList.toggle("active", false);
 
       let target = document.getElementById(btn.dataset.target);
-      target.classList.toggle("tab__content--hidden", true);
+      target.classList.toggle("collapse", true);
     });
 
-    button.classList.toggle("tab__button--active", true);
+    button.classList.toggle("active", true);
 
     let target = document.getElementById(button.dataset.target);
-    target.classList.toggle("tab__content--hidden", false);
+    target.classList.toggle("collapse", false);
   });
-});
-
-// toggle menu
-const menu = document.querySelector(".humberger-menu");
-
-menu.addEventListener("click", () => {
-  menu.classList.toggle("humberger-menu--open");
-  const target = document.getElementById(menu.dataset.target);
-  target.classList.toggle("nav--show");
 });
 
 // check quiz
